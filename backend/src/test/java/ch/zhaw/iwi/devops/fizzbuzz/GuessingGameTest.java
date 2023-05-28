b81bde164f55e423cdd8c6c2f9c260c783af4135
andreadae <100606105+andreadae@users.noreply.github.com>
2023-05-28 17:04:03 +0200

    test and method for uncorrect (default) answer

--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/GuessingGameTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/GuessingGameTest.java
@@ -1,6 +1,9 @@
 package ch.zhaw.iwi.devops.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Assertions;
 
 public class GuessingGameTest {
@@ -8,5 +11,12 @@ public class GuessingGameTest {
     public void GuessingGameCorrect() {
         GuessingGame test = new GuessingGame();
         Assertions.assertEquals("Das ist korrekt!", test.guess(10));
-    }        
+    }    
+    
+    @Test
+    public void GuessingGameDefault() {
+        GuessingGame test = new GuessingGame();
+        Assertions.assertEquals("Das ist nicht die korrekte Eingabe", test.guess(11));
+    }    
+        
 }

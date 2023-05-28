4e0c7bdac09eb8f1ecf612041b024dfdadfac868
andreadae <100606105+andreadae@users.noreply.github.com>
2023-05-28 16:33:13 +0200

    first test correct message

--- backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/GuessingGame.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/GuessingGame.java
@@ -1,5 +1,12 @@
 package ch.zhaw.iwi.devops.fizzbuzz;
 
 public class GuessingGame {
-    
+    public String guess(int number) {
+        System.out.println("Dein Guess ist" +number);
+        if (number == 10){
+            return "Das ist korrekt!";
+        }
+        return null;
+    }
+
 }
--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/GuessingGameTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/GuessingGameTest.java
@@ -1,5 +1,12 @@
 package ch.zhaw.iwi.devops.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class GuessingGameTest {
-    
+    @Test
+    public void GuessingGameCorrect() {
+        GuessingGame test = new GuessingGame();
+        Assertions.assertEquals("Das ist korrekt!", test.guess(10));
+    }        
 }
